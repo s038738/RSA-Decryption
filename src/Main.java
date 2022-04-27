be67f708b846ba6e66bf68262e30f766466890f8
s038738 <s038738@ad.viko.lt>
2022-04-27 18:45:24 +0300

    Full

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,152 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String args[]){
-        Random rand1 = new Random(System.currentTimeMillis());
-        Random rand2 = new Random(System.currentTimeMillis()*10);
+        public static void main(String args[]) throws IOException {
 
-        int pubkey = 2;
 
-        BigInteger p = BigInteger.probablePrime(32, rand1);
-        BigInteger q = BigInteger.probablePrime(32, rand2);
+            Scanner input = new Scanner(System.in);
+            boolean mainLoop = true;
 
-        BigInteger n = p.multiply(q);
+            int choice;
+            while(true){
+                System.out.println("\nMain Menu");
+                System.out.print("1.) ALL \n");
+                System.out.print("2.) Encryption.\n");
+                System.out.print("3.) Decryption.\n");
+                System.out.print("\nEnter Your Menu Choice: ");
 
-        BigInteger p_1=p.subtract(new BigInteger("1"));
-        BigInteger q_1=q.subtract(new BigInteger("1"));
+                choice = input.nextInt();
 
-        BigInteger z=p_1.multiply(q_1);
 
-        while(true){
-            BigInteger GCD = z.gcd(new BigInteger(""+pubkey));
-            if(GCD.equals(BigInteger.ONE)){
-                break;
+                switch(choice){
+
+                    case 1:
+                        Scanner sc = new Scanner(System.in);
+                        System.out.println("Enter text: ");
+                        String msg = sc.nextLine();
+                        System.out.println("Enter P value: ");
+                        BigInteger p = sc.nextBigInteger();
+                        System.out.println("IEnter Q value: ");
+                        BigInteger q = sc.nextBigInteger();
+
+
+                        FileWriter myWriter = new FileWriter("Ciphered_Value.txt");
+
+                        BigInteger n = p.multiply(q);
+
+                        var e = BigInteger.valueOf(0x10001);
+                        System.out.println("P Value: " + p + "\nQ Value: " + q);
+                        var phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+                        //System.out.println(phi);
+
+                        var d = e.modInverse(phi);
+                        System.out.println("Public key: " + e + "\nn Value: " + n);
+
+
+                        byte[] bytes = msg.getBytes();
+
+                        int i_decypherVal = 0;
+                        for (int i = 0; i < msg.length(); i++) {
+
+                            int asciiVal = bytes[i];
+                            BigInteger val = new BigInteger(String.valueOf(asciiVal));
+                            BigInteger cipherVal = val.modPow(e, n);
+
+                            BigInteger decypherVal = cipherVal.modPow(d, n);
+                            i_decypherVal = decypherVal.intValue();
+
+                            System.out.println("\nMessage char : " + msg.charAt(i) +
+                                    "\nAscii number: " + asciiVal +
+                                    "\nCyphered text: " + cipherVal +
+                                    "\nDeciphered text: " + Character.toString((char) i_decypherVal)+
+                            "\nDeciphered value: " + i_decypherVal);
+                            myWriter.write(String.valueOf(cipherVal) + " ");
+
+                        }
+                        myWriter.close();
+
+                        BigInteger [] bigInteger = new BigInteger[bytes.length];
+                        String actual = Files.readString(Path.of("Ciphered_Value.txt"));
+                        String [] numbers  = actual.split(" ");
+                        for (int i = 0;i<numbers.length;i++){
+                            bigInteger[i]=new BigInteger(numbers[i]);
+                        }
+
+                        byte [] bytes2 = new byte[bigInteger.length];
+                        for (int i = 0; i <bytes2.length;i++){
+                            bytes2[i]=bigInteger[i].modPow(d,n).byteValue();
+                        }
+                        String str = new String(bytes2);
+                        System.out.println("\nDeciphered text: "+str);
+
+                        break;
+                    case 2:
+                        sc = new Scanner(System.in);
+                        System.out.println("Enter text: ");
+                        msg = sc.nextLine();
+                        System.out.println("Enter P value: ");
+                        p = sc.nextBigInteger();
+                        System.out.println("IEnter Q value: ");
+                        q = sc.nextBigInteger();
+
+                        n = p.multiply(q);
+
+                        e = BigInteger.valueOf(0x10001);
+                        phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+
+                        System.out.println("Public key: " + e + "\nn Value: " + n);
+
+                        bytes = msg.getBytes();
+
+                        for (int i = 0; i < msg.length(); i++) {
+
+                            int asciiVal = bytes[i];
+                            BigInteger val = new BigInteger(String.valueOf(asciiVal));
+                            BigInteger cipherVal = val.modPow(e, n);
+
+                            System.out.println("\nMessage: " + msg.charAt(i) +
+                                    "\nCyphered text: " + cipherVal);
+                        }
+                        break;
+                    case 3:
+                        sc = new Scanner(System.in);
+                        System.out.println("Enter text: ");
+                        msg = sc.nextLine();
+                        System.out.println("Enter P value: ");
+                        p = sc.nextBigInteger();
+                        System.out.println("IEnter Q value: ");
+                        q = sc.nextBigInteger();
+
+                        n = p.multiply(q);
+                        e = BigInteger.valueOf(0x10001);
+
+                        phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+
+                        d = e.modInverse(phi);
+                        System.out.println("Public key: " + e + "\nn Value: " + n);
+
+                        int i = Integer.parseInt(msg);
+                        BigInteger bigInteger1 = BigInteger.valueOf(i);
+                        bigInteger1 = bigInteger1.modPow(d,n);
+                        int ans = bigInteger1.intValue();
+                        System.out.println("\nDeciphered Char: " + Character.toString((char) ans));
+
+                        break;
+                    default :
+                        System.out.println("This is not a valid Menu Option! Please Select Another");
+                        break;
+
+                }
+
             }
-                pubkey ++;
-        }
-
-        BigInteger big_pubkey = new BigInteger(""+pubkey);
-        BigInteger prvKey = big_pubkey.modInverse(z);
-        System.out.println("public key " + pubkey+", "+n);
-        System.out.println("private key " + prvKey+", "+n);
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the message");
-        String msg = sc.nextLine();
-
-        byte[] bytes = msg.getBytes();
-
-        for (int i=0; i<msg.length();i++){
-
-            int asciiVal=bytes[i];
-            BigInteger val = new BigInteger(String.valueOf(asciiVal));
-            BigInteger cipherVal=val.modPow(big_pubkey,n);
-            //System.out.println("ciphered text:"+cipherVal);
-
-            BigInteger decypherVal = cipherVal.modPow(prvKey,n);
-            int i_decypherVal = decypherVal.intValue();
-            //System.out.println("plaint text: " + Character.toString((char)i_plainVal));
-
-            System.out.println("\nMessage: " + msg.charAt(i) +
-                    ", Ascii number: " + asciiVal+
-                    "\nCyphered text: "+ cipherVal +
-                                "\nDecyphered text: " + Character.toString((char)i_decypherVal));
 
         }
-    }
 }
